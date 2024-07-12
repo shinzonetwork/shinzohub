ae7020a0c248363c72520af7109eaba2761ed5c7
Roy Lee <roylee17@gmail.com>
2024-07-12 14:05:25 -0700

    tier: update testutil

--- testutil/sample/sample.go
+++ testutil/sample/sample.go
@@ -1,6 +1,7 @@
 package sample
 
 import (
+	"github.com/cometbft/cometbft/crypto/secp256k1"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -10,4 +11,18 @@ func AccAddress() string {
 	pk := ed25519.GenPrivKey().PubKey()
 	addr := pk.Address()
 	return sdk.AccAddress(addr).String()
+}
+
+// RandomAccAddress returns a sample account address
+func RandomAccAddress() sdk.AccAddress {
+	pk := ed25519.GenPrivKey().PubKey()
+	pkAddr := pk.Address()
+	accAddr := sdk.AccAddress(pkAddr)
+	return accAddr
+}
+
+// RandomValAddress generates a random ValidatorAddress for simulation
+func RandomValAddress() sdk.ValAddress {
+	valPub := secp256k1.GenPrivKey().PubKey()
+	return sdk.ValAddress(valPub.Address())
 }
