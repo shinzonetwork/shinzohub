cd376ea0067c6a2a21a62472737b5864f22d8cae
Ivan <ivanverch@gmail.com>
2024-11-20 18:52:10 +0700

    Update scripts, Makefile, and devnet config

--- scripts/genesis-setup.sh
+++ scripts/genesis-setup.sh
@@ -9,7 +9,7 @@ BIN="build/sourcehubd"
 $BIN init $NODE_NAME --chain-id $CHAIN_ID
 
 $BIN keys add $VALIDATOR --keyring-backend test
-VALIDATOR_ADDR=$(build/sourcehubd keys show $VALIDATOR -a --keyring-backend test)
+VALIDATOR_ADDR=$($BIN keys show $VALIDATOR -a --keyring-backend test)
 $BIN genesis add-genesis-account $VALIDATOR_ADDR 100000000000open
 $BIN genesis gentx $VALIDATOR 100000000open --chain-id $CHAIN_ID --keyring-backend test
 
