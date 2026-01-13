4d2af0c82a6ea801dac2bcf4c6200e59f7079e64
Bruno Gomes <gomes.bruno.ac@gmail.com>
2026-01-13 14:36:38 -0300

    policy refactor

--- x/sourcehub/keeper/policy.go
+++ x/sourcehub/keeper/policy.go
@@ -1,6 +1,8 @@
 package keeper
 
 import (
+	_ "embed"
+
 	"github.com/cosmos/cosmos-sdk/runtime"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/shinzonetwork/shinzohub/x/sourcehub/types"
@@ -20,113 +22,5 @@ func (k Keeper) GetPolicyId(ctx sdk.Context) string {
 	return string(bz)
 }
 
-var policy = `
-name: shinzo
-resources:
-  primitive:
-    relations:
-      admin:
-        manages:
-        - writer
-        - syncer
-        - subscriber
-        - banned
-        types:
-        - actor
-        - group->administrator
-      writer:
-        types:
-        - actor
-        - group->member
-      syncer:
-        types:
-        - actor
-        - group->member
-      subscriber:
-        types:
-        - actor
-      banned:
-        types:
-        - actor
-    permissions:
-      sync:
-        expr: (writer + syncer + subscriber) - banned
-      update:
-        expr: writer - banned
-      delete:
-        expr: owner
-      read:
-        expr: (subscriber) - banned
-  view:
-    relations:
-      admin:
-        manages:
-        - creator
-        - writer
-        - syncer
-        - subscriber
-        - parent
-        - banned
-        types:
-        - actor
-        - group->administrator
-      creator:
-        types:
-        - actor
-      writer:
-        types:
-        - actor
-        - group->member
-      syncer:
-        types:
-        - actor
-        - group->member
-      banned:
-        types:
-        - actor
-      subscriber:
-        types:
-        - actor
-      parent:
-        types:
-        - primitive
-        - view
-    permissions:
-      sync:
-        expr: writer + syncer + parent->sync + subscriber - banned
-      update:
-        expr: ((writer + parent->update)) - banned
-      read:
-        expr: (subscriber) - banned
-      delete:
-        expr: owner
-      created:
-        expr: creator - banned
-  group:
-    relations:
-      owner:
-        manages:
-        - admin
-        - guest
-        - blocked
-        types:
-        - actor
-      admin:
-        manages:
-        - guest
-        - blocked
-        types:
-        - actor
-        - group->administrator
-      guest:
-        types:
-        - actor
-      blocked:
-        types:
-        - actor
-    permissions:
-      member:
-        expr: guest - blocked
-      administrator:
-        expr: (owner + admin) - blocked
-`
+//go:embed policy.yaml
+var policy string
---
+++ x/sourcehub/keeper/test_valid_policy.go
@@ -0,0 +1,33 @@
+package keeper
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/sourcenetwork/acp_core/pkg/runtime"
+	"github.com/sourcenetwork/acp_core/pkg/services"
+	"github.com/sourcenetwork/acp_core/pkg/types"
+)
+
+// Test ensure the YAML ACP Policy does not become stale,
+// by trying to create it with against the acp engine directly
+func Test_ShinzoYamlPolicyIsValid(t *testing.T) {
+	pol, err := os.ReadFile("policy.yaml")
+	require.NoError(t, err)
+
+	manager, err := runtime.NewRuntimeManager(
+		runtime.WithMemKV(),
+	)
+	require.NoError(t, err)
+
+	engine := services.NewACPEngine(manager)
+
+	ctx := context.Background()
+	_, err = engine.CreatePolicy(ctx, &types.CreatePolicyRequest{
+		Policy: string(pol),
+	})
+	require.NoError(t, err)
+}
